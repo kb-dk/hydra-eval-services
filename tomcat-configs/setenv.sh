a221720a5019964a3e8f3d90ba178fbb752f0a13
next-gen doms <dims@hydra-eval.statsbiblioteket.dk>
2015-10-20 12:10:13 +0200

    stop overwriting previous set JAVA_OPTS

--- tomcat-configs/setenv.sh
+++ tomcat-configs/setenv.sh
@@ -3,7 +3,7 @@ JAVA_OPTS="$JAVA_OPTS -Dsolr.solr.home=/home/dims/services/solr"
 JAVA_OPTS="$JAVA_OPTS -Dsolr.data.dir=/dims-var/solr"
 
 #Opts for fcrepo
-JAVA_OPTS="-Djava.awt.headless=true -Dfile.encoding=UTF-8 -server -Xms512m -Xmx1024m -XX:NewSize=256m -XX:MaxNewSize=256m -XX:PermSize=256m -XX:MaxPermSize=256m"
+JAVA_OPTS="$JAVA_OPTS -Djava.awt.headless=true -Dfile.encoding=UTF-8 -server -Xms512m -Xmx1024m -XX:NewSize=256m -XX:MaxNewSize=256m -XX:PermSize=256m -XX:MaxPermSize=256m"
 JAVA_OPTS="$JAVA_OPTS -Dfcrepo.home=/dims-var/fedora/data"
 JAVA_OPTS="$JAVA_OPTS -Dfcrepo.audit.container=/dims-var/fedora/audit"
 
